f512901df7b10c2f2fde07de3214901e8073e3bf
MateoCaicedoW <mcaicedo@wawand.co>
2024-09-20 17:49:32 -0500

    Refactor SSE render function to use template.New instead of template.Must

--- internal/sse/render.go
+++ internal/sse/render.go
@@ -3,24 +3,14 @@ package sse
 import (
 	"bytes"
 	"html/template"
-	"io"
 )
-
-var path = "internal/sse"
-
-var templates = template.Must(template.ParseGlob(path + "/*.html"))
-
-// Render renders a template document
-func render(w io.Writer, name string, data interface{}) error {
-	return templates.ExecuteTemplate(w, name, data)
-}
 
 // RenderToString renders a template document to a string
 func RenderToString(name string, data interface{}) (string, error) {
 	var buf []byte
 	w := bytes.NewBuffer(buf)
 
-	err := render(w, name, data)
+	err := template.Must(template.New(name).ParseFiles("internal/sse/"+name)).Execute(w, data)
 	if err != nil {
 		return "", err
 	}
