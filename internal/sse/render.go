ae942d61bc82ce3e9f06e89e062d944aa783e123
MateoCaicedoW <mcaicedo@wawand.co>
2024-09-23 08:54:56 -0500

    Refactor SSE render function to use embedded templates

--- internal/sse/render.go
+++ internal/sse/render.go
@@ -2,16 +2,20 @@ package sse
 
 import (
 	"bytes"
+	"embed"
 	"fmt"
 	"html/template"
 )
+
+//go:embed *.html
+var tmpls embed.FS
 
 // RenderToString renders a template document to a string
 func RenderToString(name string, data interface{}) (string, error) {
 	var buf []byte
 	w := bytes.NewBuffer(buf)
 
-	err := template.Must(template.New(name).ParseFiles("internal/sse/"+name)).Execute(w, data)
+	err := template.Must(template.New(name).ParseFS(tmpls, name)).Execute(w, data)
 	if err != nil {
 		fmt.Println("Error rendering template:", err)
 		return "", err
